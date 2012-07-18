348fe177beca27700753b37f13c30d753445e5a8
Robert M Ochshorn <mail@RMOZONE.COM>
2012-07-18 22:27:58 +0545

    fixed motion-compensated threshold with web preview

--- why/contours60.py
+++ why/contours60.py
@@ -1,33 +1,36 @@
 import cv2
 from cvframes import video_frames
 from numpy import uint8
+from numm import np2image, image2np
 import pickle
 
 BLUR = 2
-# ???
-A = 45
-B = 45
-
+THRESH = 60
+ENHANCE_MOTION = 1.0
 DILATION = 3
 
 def contours(src, dst):
     acc = []
-    ADAPTIVE = True
-    for idx,fr in enumerate(video_frames(src, height=None)):
-        if idx == 0:
-            if fr.mean() < 30:
-                print 'Very dark: not attempting an adaptive threshold.'
-                ADAPTIVE = False
 
-        fr = fr.mean(axis=2)
+    comp = 255 - image2np(src + '.comp.png').mean(axis=2)
+
+    for idx,fr in enumerate(video_frames(src, height=None)):
+        fr = 255 - fr.mean(axis=2)
+        fr = (fr - ENHANCE_MOTION*comp).clip(0,255)
         cv2.blur(fr, (BLUR, BLUR), fr)
-        fr = fr.astype(uint8)
-        if ADAPTIVE:
-            fr = cv2.adaptiveThreshold(fr, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, A, B)
-        else:
-            fr = 255*(fr>60).astype(uint8)
+
+        # fill dynamic range
+        fr = fr-fr.min()
+        if fr.max() > 0:
+            fr *= (255.0/fr.max())
+
+
+        fr = 255*(fr>THRESH).astype(uint8)
 
         fr = cv2.dilate(fr, None, iterations=DILATION)
+
+        if idx == 0:
+            np2image(fr, dst.replace('.pkl', '.png'))
 
         contours, hierarchy = cv2.findContours(fr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         acc.append(contours)
@@ -36,4 +39,4 @@ def contours(src, dst):
 
 if __name__=='__main__':
     import sys
-    contours(sys.argv[1], sys.argv[2])
+    contours(sys.argv[1], sys.argv[-1])
