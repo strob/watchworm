2efa1966759dd7d9d6ef2e13d9a396d90933c0df
Robert M Ochshorn <mail@RMOZONE.COM>
2012-07-23 00:36:57 +0545

    ruledcontour & fps

--- why/jsonification.py
+++ why/jsonification.py
@@ -5,10 +5,7 @@ import pickle
 import numpy
 
 rec = {}
-p45 = {}
-p60 = {}
-p50 = {}
-p75 = {}
+allpaths = {}
 
 def motion(path):
     dists = path[1:] - path[:-1]
@@ -18,45 +15,49 @@ def motion(path):
 if __name__=='__main__':
 
     for f in glob.glob('data/*.avi'):
+
+        FPS = get_fps(f)
+
         base = os.path.basename(f)
         rec[base] = {"filename": os.path.basename(f)}
 
-        for string, store in [['ADP45', p45], ['MOT60', p60], ['MBL50', p50], ['MBL75', p75]]:
-            paths = pickle.load(open('%s.path%s.pkl' % (f, string)))
-            docs = []
-            for idx,(path,smoothed) in enumerate(paths):
-                arr = numpy.array(path)
-                raw_motion = motion(arr[:,:2])
-                # smoothed = smooth(arr)
-                amnt_motion = motion(smoothed)
+        paths = pickle.load(open('%s.path.pkl' % (f)))
+        docs = []
 
-                distance = numpy.hypot(*(arr[0,:2] - arr[-1,:2]))
+        store = allpaths
 
-                avg_speed = amnt_motion / float(arr[-1,-1]) # px/frame
+        for idx,(path,smoothed) in enumerate(paths):
+            arr = numpy.array(path)
+            raw_motion = motion(arr[:,:2])
+            # smoothed = smooth(arr)
+            amnt_motion = motion(smoothed)
 
-                uid = '%s-%s-%d' % (base, string, idx)
-                doc = {"recording": base,
-                       "method": string,
-                       "center": path[0][:2],
-                       "radius": path[0][2],
-                       "area": path[0][3],
-                       "raw_motion": raw_motion,
-                       "motion": amnt_motion,
-                       "distance": distance,
-                       "speed": avg_speed}
-                docs.append(doc)
-                store[uid] = doc
+            distance = numpy.hypot(*(arr[0,:2] - arr[-1,:2]))
 
-            # compute recording-wide averages
-            recinfo = {}
+            avg_speed = FPS*amnt_motion / float(arr[-1,-1]) # px/sec
 
-            nworms = len(paths)
-            recinfo["nworms_"+string] = nworms
+            uid = '%s-%d' % (base, idx)
+            doc = {"recording": base,
+                   "center": path[0][:2],
+                   "radius": path[0][2],
+                   "area": path[0][3],
+                   "raw_motion": raw_motion,
+                   "motion": amnt_motion,
+                   "distance": distance,
+                   "speed": avg_speed}
+            docs.append(doc)
+            store[uid] = doc
 
-            for key in ["radius", "area", "raw_motion", "motion", "distance", "speed"]:
-                recinfo[key+"_"+string] = sum([X[key] for X in docs])/float(nworms)
+        # compute recording-wide averages
+        recinfo = {}
+
+        nworms = len(paths)
+        recinfo["nworms_"+string] = nworms
+
+        for key in ["radius", "area", "raw_motion", "motion", "distance", "speed"]:
+            recinfo[key+"_"+string] = sum([X[key] for X in docs])/float(nworms)
             
-            rec[base].update(recinfo)
-            json.dump(store, open("Path%s.json" % (string), 'w'))
+        rec[base].update(recinfo)
 
+    json.dump(allpaths, open("Path.json", 'w'))
     json.dump(rec, open("Recording.json", 'w'))
--- why/showmotion.py
+++ why/showmotion.py
@@ -1,56 +1,78 @@
-from cvframes import video_frames
+from cvframes import video_frames, get_fps
 from numm.video import VideoWriter
 import numpy
 import cv2
 import pickle
 from jsonification import motion # this should be elsewhere ...
 
-def showmotion(src, paths, dest):
+def showmotion(orig, src, paths, dest):
+    FPS = get_fps(orig)
+
     paths = pickle.load(open(paths))
 
     # divide by number of frames
-    avg_speeds = [motion(y)/paths[idx][-1][0][-1] for idx,(x,y) in enumerate(paths)]
+    # px/second
+    avg_speeds = [FPS*motion(y)/paths[idx][-1][0][-1] for idx,(x,y) in enumerate(paths)]
+    avg_speed = sum(avg_speeds) / len(avg_speeds)
 
     vout = None
     fr = None
+    orig_vout = None
+    orig_fr = None
+
+    orig_vf = video_frames(orig, height=None);
 
     for idx,ofr in enumerate(video_frames(src, height=None)):
+        orig_ofr = orig_vf.next()
+
         if idx == 0:
             vout = VideoWriter(dest, ofr.shape)
             fr = numpy.zeros(ofr.shape, dtype=numpy.uint8)
+            orig_vout = VideoWriter(orig.replace('showmotion', 'origmotion'), orig_ofr.shape, {"fps": FPS})
+            orig_fr = numpy.zeros(orig_ofr.shape, dtype=numpy.uint8)
+
 
         fr[:] = 255-ofr             # contiguity ...
+        orig_fr[:] = orig_ofr
 
-        for p_idx,(raw,smooth) in enumerate(paths):
-            raw = filter(lambda x: x[4] <= idx, raw)
-            smooth = smooth[:max(0,len(raw)-5)]
+        for vo, f in [[orig_vout, orig_fr], [vout, fr]]:
 
-            raw = numpy.array(raw)
-            pts = raw[:,:2].astype(numpy.int32)
-            cv2.polylines(fr, [pts], False, (255, 0, 0))
+            speeds = []
 
-            tup = tuple(raw[-1,:2].astype(int).tolist())
-            cv2.circle(fr, tup, raw[-1,2].astype(int), (255, 0, 0))
-            cv2.circle(fr, tup, 3, (255, 0, 0))
+            for p_idx,(raw,smooth) in enumerate(paths):
+                raw = filter(lambda x: x[4] <= idx, raw)
+                smooth = smooth[:max(0,len(raw)-5)]
 
-            speed = 0
-            if len(pts) > 1:
-                speed = numpy.hypot(*(pts[-1] - pts[-2]).T)
-            txt = "%.2f (%.2f)" % (avg_speeds[p_idx], speed)
-            cv2.putText(fr, txt, tup, cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
+                raw = numpy.array(raw)
+                pts = raw[:,:2].astype(numpy.int32)
+                cv2.polylines(f, [pts], False, (255, 0, 0))
 
-            # & smoothed
-            if len(smooth) > 0:
-                pts = numpy.array(smooth).astype(numpy.int32)
-                cv2.polylines(fr, [pts], False, (0, 0, 255))
+                tup = tuple(raw[-1,:2].astype(int).tolist())
+                cv2.circle(f, tup, raw[-1,2].astype(int), (255, 0, 0))
+                cv2.circle(f, tup, 3, (255, 0, 0))
 
-                tup = tuple(pts[-1].astype(int).tolist())
-                cv2.circle(fr, tup, 3, (0, 0, 255))
+                speed = 0
+                if len(pts) > 1:
+                    speed = numpy.hypot(*(pts[-1] - pts[-2]).T)
+                speeds.append(speed)
+                txt = "%.2f (%.2f)" % (avg_speeds[p_idx], speed)
+                cv2.putText(f, txt, tup, cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
 
+                # & smoothed
+                if len(smooth) > 0:
+                    pts = numpy.array(smooth).astype(numpy.int32)
+                    cv2.polylines(f, [pts], False, (0, 0, 255))
 
-        vout.write(fr)
+                    tup = tuple(pts[-1].astype(int).tolist())
+                    cv2.circle(f, tup, 3, (0, 0, 255))
+
+            txt = "%.2f (%.2f), N=%d" % (avg_speed, sum(speeds)/len(speed), len(paths))
+            cv2.putText(f, txt, (30,30), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,255))
+
+            vo.write(f)
 
     vout.close()
+    orig_vout.close()
             
 
 if __name__=='__main__':
