65a23cb1e457814dc7f21e0837d10f12d8ceb253
Robert M Ochshorn <mail@RMOZONE.COM>
2012-07-28 19:36:50 +0545

    Remove unused files

--- watch/server/watch.js
+++
@@ -1,3 +0,0 @@
-Recording = new Meteor.Collection("recording");
-Preview = new Meteor.Collection("preview");
-Worm = new Meteor.Collection("worm");
--- worms/names.py
+++
@@ -1,9 +0,0 @@
-import random
-
-names = open("NAMES.txt").read().split("\n")
-
-def next():
-    return random.choice(names)
-
-def jumble():
-    return next().split()[0] + ' ' + next().split()[-1]
