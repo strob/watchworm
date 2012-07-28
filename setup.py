d996d3d05b625f16f7885b46ca0cccf18629aa31
Robert M Ochshorn <mail@RMOZONE.COM>
2012-07-28 21:25:39 +0545

    Make watchworm module

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import distutils.core
+distutils.core.setup(
+    name='watchworm',
+    version='7.0',
+    url='http://github.com/strob/watchworm',
+    description='why watch worms?',
+    author='Robert M Ochshorn',
+    author_email='mail@RMOZONE.COM',
+    packages=['watchworm'],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords='opencv blobs tracking motion analysis',
+    license='GPL')
